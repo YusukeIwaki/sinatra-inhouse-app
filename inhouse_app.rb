c97ac8d02dbde9d962feec9a47e9a646743ba2f0
YusukeIwaki <iwaki@i3-systems.com>
2020-10-08 00:44:51 +0900

    authorization

--- inhouse_app.rb
+++ inhouse_app.rb
@@ -6,13 +6,38 @@ class InhouseApp < Sinatra::Base
     provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
   end
 
+  helpers do
+    def authorize!
+      unless authenticated?
+        redirect to('/auth/github')
+      end
+      unless authorized?
+        halt 403, 'Forbidden. <a href="/logout">Logout</a>'
+      end
+    end
+
+    def authenticated?
+      session[:user]
+    end
+
+    def authorized?
+      session[:user] == 'YusukeIwaki'
+    end
+  end
+
   get '/' do
+    authorize!
     '<h1>It works!</h1>'
   end
 
   get '/auth/github/callback' do
     auth_hash = request.env['omniauth.auth']
-    puts auth_hash
+    session[:user] = auth_hash['extra']['raw_info']['login']
+    redirect to('/')
+  end
+
+  get '/logout' do
+    session[:user] = nil
     redirect to('/')
   end
 end
