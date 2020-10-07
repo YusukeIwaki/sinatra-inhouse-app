5331060c4feaf748736347044863870ff007bc6f
YusukeIwaki <iwaki@i3-systems.com>
2020-10-07 23:49:17 +0900

    GitHubログイン

--- inhouse_app.rb
+++ inhouse_app.rb
@@ -1,7 +1,18 @@
 require 'sinatra/base'
 
 class InhouseApp < Sinatra::Base
+  use Rack::Session::Cookie
+  use OmniAuth::Builder do
+    provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
+  end
+
   get '/' do
     '<h1>It works!</h1>'
+  end
+
+  get '/auth/github/callback' do
+    auth_hash = request.env['omniauth.auth']
+    puts auth_hash
+    redirect to('/')
   end
 end
